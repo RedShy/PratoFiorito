f8f1b28d151f37fa37cb6208a508b964899c760a
Mastro <Mastro@DESKTOP-SUH69HG>
2018-11-19 20:16:13 +0100

    modificate lobby

--- PratoFiorito/src/pratofiorito/components/services/LobbyService.java
+++ PratoFiorito/src/pratofiorito/components/services/LobbyService.java
@@ -7,19 +7,18 @@ import org.springframework.stereotype.Service;
 
 import pratofiorito.domain.Lobby;
 
-
 @Service
 public class LobbyService
 {
-	//TODO fare locking concorrente
+	// TODO fare locking concorrente
 	private List<Lobby> lobbies = new ArrayList<>();
-	
+
 	public List<Lobby> getLobbies()
 	{
-		//TODO
-		ArrayList<Lobby> list= new ArrayList<>();
-		list.add(new Lobby("lobby1"));
-		list.add(new Lobby("lobby2"));
+		// TODO
+		ArrayList<Lobby> list = new ArrayList<>();
+		list.add(new Lobby("ciccop"));
+		list.add(new Lobby("provoletta"));
 		list.add(new Lobby("lobby3"));
 		return list;
 	}
