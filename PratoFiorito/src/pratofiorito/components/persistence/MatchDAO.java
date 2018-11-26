25916b387bb626fc1bbd1d531757724d0c64a5f2
Mastro <Mastro@DESKTOP-SUH69HG>
2018-11-26 16:36:40 +0100

    staccati dao

--- PratoFiorito/src/pratofiorito/components/persistence/MatchDAO.java
+++ PratoFiorito/src/pratofiorito/components/persistence/MatchDAO.java
@@ -1,34 +1,32 @@
 package pratofiorito.components.persistence;
 
-import javax.annotation.PostConstruct;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
-import pratofiorito.domain.User;
-
-@Repository
-public class MatchDAO {
+//@Repository
+public class MatchDAO
+{
 	@Autowired
 	private SessionFactory sessionFactory;
-	
-	public void save(Math match) {
+
+	public void save(Math match)
+	{
 		Session session = sessionFactory.openSession();
 
 		Transaction tx = null;
-		try {
-			tx= session.beginTransaction();
+		try
+		{
+			tx = session.beginTransaction();
 			session.save(match);
 			tx.commit();
 
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			tx.rollback();
 		}
 		session.close();
 	}
-
 
 }
--- PratoFiorito/src/pratofiorito/components/persistence/UserDAO.java
+++ PratoFiorito/src/pratofiorito/components/persistence/UserDAO.java
@@ -7,65 +7,64 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
 import pratofiorito.domain.User;
 
-@Repository
-public class UserDAO {
+//@Repository
+public class UserDAO
+{
 	@Autowired
 	private SessionFactory sessionFactory;
 
-	
-	
 	@PostConstruct
-	public void init() {
+	public void init()
+	{
 		/*
-		User u1= new User();
-		u1.setUsername("ciccio");
-		u1.setPassword("ciccio");
-		save(u1);
-		User u2= new User();
-		u2.setUsername("franco");
-		u2.setPassword("franco");
-		save(u1);
-		*/
+		 * User u1= new User(); u1.setUsername("ciccio");
+		 * u1.setPassword("ciccio"); save(u1); User u2= new User();
+		 * u2.setUsername("franco"); u2.setPassword("franco"); save(u1);
+		 */
 	}
 
-	public boolean save(User user) {
+	public boolean save(User user)
+	{
 		Session session = sessionFactory.openSession();
-		boolean error=false;
+		boolean error = false;
 		Transaction tx = null;
-		try {
-			tx= session.beginTransaction();
+		try
+		{
+			tx = session.beginTransaction();
 			session.save(user);
 			tx.commit();
 
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			tx.rollback();
-			error=true;
+			error = true;
 		}
 		session.close();
 		return error;
 	}
 
-	public boolean exists(User user) {
+	public boolean exists(User user)
+	{
 		Session openSession = sessionFactory.openSession();
-		Query<User> query = openSession.createQuery("from User as us where us.username=:u and us.password=:p", User.class)
-			.setParameter("u", user.getUsername()).setParameter("p", user.getPassword());
-		
+		Query<User> query = openSession
+				.createQuery("from User as us where us.username=:u and us.password=:p", User.class)
+				.setParameter("u", user.getUsername()).setParameter("p", user.getPassword());
+
 		boolean result = query.uniqueResult() != null;
 		openSession.close();
 		return result;
 	}
-	
-	public User getUser(String username) {
+
+	public User getUser(String username)
+	{
 		Session openSession = sessionFactory.openSession();
 		Query<User> query = openSession.createQuery("from User as us where us.username=:u ", User.class)
-			.setParameter("u", username);
-		
+				.setParameter("u", username);
+
 		return query.uniqueResult();
 	}
-	
 
 }
